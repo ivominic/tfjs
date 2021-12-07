9161861f625feb438d55d79e39f30cdec0f0319f
Ivo <ivo.minic@gmail.com>
2021-12-07 15:35:54 +0100

    Adding train data

--- js/index.js
+++ js/index.js
@@ -30,20 +30,69 @@
 //b.dispose()
 //b.transpose().print();
 
-const y = tf.tidy(() => {
-  // a, b, and one will be cleaned up when the tidy ends.
-  const one = tf.scalar(1);
-  const a = tf.scalar(2);
-  const b = a.square();
+// const y = tf.tidy(() => {
+//   // a, b, and one will be cleaned up when the tidy ends.
+//   const one = tf.scalar(1);
+//   const a = tf.scalar(2);
+//   const b = a.square();
 
-  console.log("numTensors (in tidy): " + tf.memory().numTensors);
+//   console.log("numTensors (in tidy): " + tf.memory().numTensors);
 
-  // The value returned inside the tidy function will return
-  // through the tidy, in this case to the variable y.
-  return b.add(one);
-});
-console.log("numTensors (outside tidy): " + tf.memory().numTensors);
-y.print();
-y.dispose();
+//   // The value returned inside the tidy function will return
+//   // through the tidy, in this case to the variable y.
+//   return b.add(one);
+// });
+// console.log("numTensors (outside tidy): " + tf.memory().numTensors);
+// y.print();
+// y.dispose();
 
 //y.print();
+
+const model = tf.sequential();
+
+const h1 = tf.layers.dense({
+  units: 4,
+  inputShape: [2],
+  activation: "sigmoid",
+});
+
+model.add(h1);
+
+const output = tf.layers.dense({
+  units: 1,
+  activation: "sigmoid",
+});
+
+model.add(output);
+
+const opt = tf.train.sgd(0.1);
+
+model.compile({
+  optimizer: opt,
+  loss: tf.losses.meanSquaredError,
+});
+
+const xs = tf.tensor2d([
+  [0, 0],
+  [0.5, 0.5],
+  [1, 1],
+]);
+
+const ys = tf.tensor2d([[1], [0.5], [0]]);
+
+train().then(() => {
+  let output = model.predict(xs);
+
+  console.log("Training Complete");
+  output.print();
+});
+
+async function train() {
+  for (let i = 0; i < 1000; i++) {
+    const config = {
+      shuffle: true,
+    };
+    const resp = await model.fit(xs, ys);
+    console.log(resp.history.loss[0]);
+  }
+}
